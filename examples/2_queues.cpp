2411fd5d83d0a1a8892e905486b8f16dcd50bf3a
Kris Rowe <kris.rowe@anl.gov>
2022-06-24 21:58:50 +0000

    Simplify queues example.

--- examples/2_queues.cpp
+++ examples/2_queues.cpp
@@ -1,36 +1,12 @@
 #include <iostream>
-#include <limits>
-#include <random>
 #include <vector>
 
 #include "CL/sycl.hpp"
 
-// Check for (near) equality between two floating-point arrays
-// Note: please be more rigorous than this in production code!
-bool nearlyEqual(const std::vector<double>& lhs,const std::vector<double>& rhs) {
-  for (std::size_t i{}; i < lhs.size(); ++i) {
-    const double& expected = lhs[i];
-    const double& actual = rhs[i];
-    if (std::abs(expected - actual) >
-        std::numeric_limits<double>::epsilon() * std::abs(expected + actual)) {
-      return false;
-    }
-  }
-  return true;
-}
-
 int main() {
   const std::size_t vector_length = 100;
-  std::vector<double> host_original(vector_length);
+  std::vector<double> host_original(vector_length,1.0);
   std::vector<double> host_copy(vector_length);
-
-  // Create a vector of random values on the host
-  std::random_device seed;
-  std::mt19937_64 generator{seed()};
-  std::uniform_real_distribution<double> distribution(-1.0, 1.0);
-  for (auto& x : host_original) {
-    x = distribution(generator);
-  }
 
   // Create a sycl::queue using the default device selector
   sycl::device sycl_device{sycl::default_selector()};
@@ -42,19 +18,23 @@ int main() {
       sycl::malloc_device<double>(vector_length, sycl_device, sycl_context);
 
   // Copy the vector of values from the host to the device
-  sycl_queue.copy(host_original.data(), device_vector, vector_length);
+  sycl_queue.copy(host_original.data(), device_vector, host_original.size());
 
   // Sychronize by waiting for all enqueued tasks to complete
   sycl_queue.wait();
 
   // Copy the values from the device back to the host
-  sycl_queue.copy(device_vector, host_copy.data(), vector_length);
+  sycl_queue.copy(device_vector, host_copy.data(), host_copy.size());
   sycl_queue.wait();
 
-  if(!nearlyEqual(host_original,host_copy)) {
-    std::cout << "Verification failed!\n";
-    return EXIT_FAILURE;
-  }  
+  // Verify the results.
+  for(const auto& x : host_copy) {
+    // Don't check for equality of floating-point values in production code!
+    if(1.0 != x) {
+      std::cout << "Verification failed!\n";
+      return EXIT_FAILURE;
+    }  
+  }
 
   std::cout << "Success!\n";
 
